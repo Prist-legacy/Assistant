4a05b5027610f18faa1d2cc528d44e9121a09bda
PRIST LEGACY <yiigavicent@gmail.com>
2023-06-04 19:03:03 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,61 @@
+
+bot_token = "5565457816:AAG5YpM1LeiwO1-bBh478DsiNnMk4O_VMTk"
+
+
+class m:
+    ticket = 'https://ibb.co/HxdCwWQ'
+    chippercash = 'https://chipper.me/@ufmmatches'
+    CHAT_ID = '@ufmmatches' #replace your channel id
+    admin = [2036597190] # bot admins
+
+    main_channel = "ufmmatches"
+    admin_direct = "https://t.me/talktotegs"
+    main_msg = "🔘*MAIN MENU*"
+    startmsg = """
+I can help you in many things regarding to fixed games. Am smart but you can still contact the **UFM administration** for farther help.
+*So now can I know your need ?*
+Use below buttons for simplicity!👇
+    """
+    not_sub_msg = """Please subscribe to our main channel 📢 to use this BOT.
+After use /reload to proceed"""
+
+    helpmsg = "🔘HELP MENU\nYou can get help from the following.\n/how if you want to know how to book.\n/admin to talk to the admin directly.\n/verify to quickly verify you payment./menu if the menu button is not visible./free for today's free tips"
+    commands = {  # command description used in the "help" command
+    'start'       : 'Start the bot again',
+    'help'        : 'Gives you information about the available services and how to use the bot',
+    'free': 'To quickly access FREE TIPS.',
+    'reload'    : 'Reload bot to main menu 🔰 ',
+    'verify'    : 'To quickly verify your payments (Transaction ID)',
+    'menu'    : 'Tougles the main Reply buttons.',
+    'admin'    : 'Get admin attention. You can also type admin for quick access.'
+}
+
+    freetips_msg = """‼️Caution:\n
+Here at our platform, we know things are hard so we came up with the FREE TIPS department where the administrator provides you with free predictions. \nWhat does this mean, matches provided here are not 100% sure. They most times win but stake them on your own risk, losses are not on us.
+\nFor 100% sure matches, join our VIP GAMES or else you may proceed to see today's free matches👇"""
+    free_msg = "*TODAY'S FREE TIPS | {}*\n\n{}\n\n‼️_Stake accordingly_"
+    free_msg1 = "*TODAY'S FREE TIPS*\n\n*Use* /free  👈\n‼️_To get today's use command above to get the matches now_\nTo get premium highly fixed matches 👇"
+    no_free_msg = "*TODAY'S FREE TIPS | {}*\nFree matches haven't been uploaded. Wait at around *04:30pm*\n‼️_All rights reserved_"
+
+
+    vip_msg = """*CORRECT SCORE:* _This involves providing half-time and full-time score of the dedicated matches. Matches can be daily or else source hasn't confirmed it. They are 100% sure; we provide match score before it kicks off._\n*HT/FT:* _These are matches give results by halves,▫️*Halftime results to Full-time results (1/2)*👉Where home team wins half-time then away team to win full-time._
+    
+      *USE BUTTONS BELOW TO*
+      *👇CHOSE MATCH TYPE 👇*"""
+    vipmenu_msg = "VIP MATCHES are 100% sure games in that winning on these matches is guaranteed. You must stake high on them because any lose is on us where by due to any loss, we proceed with the #refund-policy"
+    cs_msg = "*Confirm* _And Proceed To Booking_" 
+    cs_msg1 = "🔘*CORRECT SCORE*\n\n*Games:* _2_\n*Odd:* •••\n*No loose:* Yes✅\n*Price:* _proceed to payments_\n\n_Use the main menu button 💰VIP-GAMES💰 then correct score to view today's ticket with actual odds_\n*Confirm* _And Proceed To Booking_"
+    cs_menu = "🔘*CORRECT SCORE*\nClosed ticket 👇"
+    country_msg = "*SPECIFY YOUR COUNTY*"
+    admin_msg = """
+🧑‍💻*ADMIN CONTACT:*
+🧔User: {}
+👤ID: `{}`
+_You be redirected to the admin's box.\n Please try to be brief and precise\n
+IE: Be on point because there are many people in that box._
+"""
+
+    vipdes_msg = "*VIP:* Is a daily subscription. This payment is done daily and verification is a vital step every each payment made.\n\n*VVIP:* Its the same as VIP but this is to those who pay for matches in a long period like weekly, monthly and yearly payments. Just multiply the VIP payments by the Days more than one, that is the VVIP subscription needed.\nVVIP members have their secret group where matches where open tickets are posted every morning plus additional bonus.\n\nHope you got it right."
+    how_msg = "*CONTACT:* [ADMIN](t.me/talktotegs) for more info"
+
+
